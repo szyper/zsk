b5bbaa54c57b490005522daa0c274f7c9303cf6e
zskgit <zskgit@gmail.com>
2022-11-03 14:39:56 +0100

    Add file

---
+++ klasa_3/C#/wykład/3D/0/project1_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Random r = new Random();
+int day = r.Next(8); // 0-7
+string content = "";
+switch (day)
+{
+    case 1: content = "poniedziałek"; break;
+    case 2: content = "wtorek"; break;
+    case 3: content = "środa"; break;
+    case 4: content = "czwartek"; break;
+    case 5: content = "piątek"; break;
+    case 6: content = "sobota"; break;
+    case 7: content = "niedziela"; break;
+    default: content = "Error!"; break;
+}
+
+Console.WriteLine("Dzień tygodnia: {0}", content);
+*/
+
+Random r = new Random();
+int day = r.Next(8); // 0-7
+string content = day switch
+{
+    1 => "poniedziałek",
+    2 => "wtorek",
+    3 => "środa",
+    4 => "czwartek",
+    5 => "piątek",
+    6 => "sobota",
+    7 => "niedziela",
+    _ => "Error!"
+};
+
+Console.WriteLine("Dzień tygodnia: {0}", content);
---
+++ klasa_3/C#/wykład/3D/project_1/Program.cs
@@ -0,0 +1,16 @@
+﻿string name = "Janusz";
+Console.WriteLine($"Witaj {name}, imię wynosi {name.Length}");
+
+
+name = "       Anna";
+Console.WriteLine($"Witaj {name}, imię wynosi {name.Length}");
+
+string startTrimName = name.TrimStart();
+Console.WriteLine($"Imię: {startTrimName}, długość: {startTrimName.Length}");
+
+
+name = "abc    ";
+// startTrimName = name.TrimStart();
+// startTrimName = name.TrimEnd();
+startTrimName = name.Trim();
+Console.WriteLine($"Imię: {startTrimName}, długość: {startTrimName.Length}");
